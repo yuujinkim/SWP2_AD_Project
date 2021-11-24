4a1870cee605fb9c2c2f69819beca355904a8624
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2021-11-24 22:06:42 +0900

    get schedule from dict

--- TODO/main.py
+++ TODO/main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from TODO.todo_controller import TODOApp
+import selenium
 # main
 app = QApplication(sys.argv)
 todo = TODOApp()
--- TODO/todo_model.py
+++ TODO/todo_model.py
@@ -1,1 +1,3 @@
 # 변수 선언
+
+dateFormat = 'yyyy-MM-dd'

7a4116d97f61ff40b37b2304811aa1aef9efad12
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2021-11-26 02:03:24 +0900

    FileNotFoundError 예외처리

--- TODO/crawl_model.py
+++ TODO/crawl_model.py
@@ -1,10 +1,14 @@
 import csv
 
 crawlingDict = {}
-f = open('data.csv', 'r', encoding='utf-8-sig')
-data = csv.reader(f)
-for line in data:
-    date = line[2].split()[0]
-    task = line[0].strip() + line[1]
-    crawlingDict[date] = [[task, line[3]]]
-f.close()
+
+try:
+    f = open('data.csv', 'r', encoding='utf-8-sig')
+    data = csv.reader(f)
+    for line in data:
+        date = line[2].split()[0]
+        task = line[0].strip() + line[1]
+        crawlingDict[date] = [[task, line[3]]]
+    f.close()
+except FileNotFoundError:
+    pass

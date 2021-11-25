1a527577acd35a0fd3198a61b4a08437ba30d249
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2021-11-25 18:36:28 +0900

    clear

--- TODO/calendar_controller.py
+++ TODO/calendar_controller.py
@@ -1,12 +1,5 @@
-from PyQt5.QtCore import QDate
-from TODO.calendar_model import dataDict
-
 # 일정 추가
 
 # 일정 삭제
 
 # 일정 가져오기
-def getDaySchedule(date):
-    for key, lst in dataDict.items():
-        if key == date:
-            return lst
